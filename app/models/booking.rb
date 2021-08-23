c9f4d273354b4fb3bbad509471be9ea73d7d0695
Tom Perkins <twperkins.work@gmail.com>
2021-08-23 18:24:21 +0100

    added validations for bookings

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,6 @@
 class Booking < ApplicationRecord
   belongs_to :spaceship
   belongs_to :user
+  validates :price, :location, :arrive, :depart, :passenger_quantity, presence: true
+  validates :destination, presence: true, inclusion: { in: %w[mars moon mercury] }
 end
