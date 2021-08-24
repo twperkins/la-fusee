507812e7b02145f4e070d9457e52d8a2a907f4f1
Khuthadzo Masindi <khuthadzo.masindi@gmail.com>
2021-08-24 13:19:54 +0100

    added global variable destination to booking model  and buttons to home page

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,6 +1,7 @@
 class Booking < ApplicationRecord
+  DESTINATION = %w[mars moon mercury]
   belongs_to :spaceship
   belongs_to :user
   validates :price, :location, :arrive, :depart, :passenger_quantity, presence: true
-  validates :destination, presence: true, inclusion: { in: %w[mars moon mercury] }
+  validates :destination, presence: true, inclusion: { in:DESTINATION }
 end
