8b0a7ff26588036befa1468fd2d91703f3985000
Tom Perkins <twperkins.work@gmail.com>
2021-08-26 16:14:06 +0100

    sorted has manies and belongs tos

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,6 +3,7 @@ class Booking < ApplicationRecord
   CAPACITY = %w[2 4 6 8]
   belongs_to :spaceship
   belongs_to :user
+  belongs_to :destination
   validates :price, :arrive, :depart, :passenger_quantity, presence: true
   validates :destination, presence: true, inclusion: { in: DESTINATION }
 end
--- app/models/destination.rb
+++ app/models/destination.rb
@@ -1,4 +1,5 @@
 class Destination < ApplicationRecord
+  has_many :bookings, dependent: :destroy
   validates :name, presence: true
   validates :flight_time, presence: true
   validates :image_url, presence: true
